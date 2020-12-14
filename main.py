6cb065e83d7af21cf43d293554529f009c49c74f
2xlink <2xlink@web.de>
2020-12-14 23:04:16 +0100

    Closes rooms after 20 minutes

--- main.py
+++ main.py
@@ -108,10 +108,10 @@ class MainHandler(tornado.web.RequestHandler):
         room_id_param = self.get_arguments("room_id")
         logging.info(f"main GET, rooms are {rooms}")
 
-        # Delete rooms which were not accessed in the last 10 minutes
+        # Delete rooms which were not accessed in the last 20 minutes
         rooms_to_delete = []
         for room_id in rooms:
-            if datetime.datetime.now() - rooms[room_id].last_access > datetime.timedelta(minutes=10):
+            if datetime.datetime.now() - rooms[room_id].last_access > datetime.timedelta(minutes=20):
                 logging.info(f"Room {room_id} not accessed for at least 10 minutes (was {rooms[room_id]})")
                 rooms_to_delete.append(room_id)
         for r in rooms_to_delete:
