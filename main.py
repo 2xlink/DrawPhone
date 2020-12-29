4ddfc712635d902e067f33e2104d82ea88fe9ee7
2xlink <2xlink@web.de>
2020-12-29 17:11:34 +0100

    Updates update notes

--- main.py
+++ main.py
@@ -616,7 +616,7 @@ def get_latest_commits():
 
     latest_commits = \
         [
-            "Adds a like button to the history. Liked prompts may be included in the future."
+            "Adds a like button to the history. Liked prompts may be included in the future.",
             "Forms can now be sent with the return key",
             "Added rename and kick functions.",
             "(Hopefully) fixed history sync."
